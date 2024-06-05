5af1fd234e98243537876eca6ddb3f929ca3491d
Abdifatah Musa <lawznleytha@gmail.com>
2024-06-05 12:07:45 +0300

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
+import React from "react";
+
+
 // Code EyesOnMe Component Here
+ function EyesOnMe (){
+    function handleFocus(event){
+        console.log("Good!")
+    }
+
+    function handleBlur(event){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+
+        </div>
+
+    )
+
+ }
+
+ export default EyesOnMe;
